156eb614558154a82dd8578c6eda6e171700efbb
Saurabh <you@example.com>
2025-05-03 13:22:13 +0530

    feat: add basic data persistence using flat file storage

--- simple-form-app/server.js
+++ simple-form-app/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app=express();
 const PORT =3000;
@@ -12,18 +13,35 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 //In memory store
-const submissions =[];
+//const submissions =[];
 
-//Handle form POST
-app.post('/submit', (req, res) => {
+//Handle form POST and stored in in-memory
+// app.post('/submit', (req, res) => {
+//     const username = req.body.username;
+//     if (username) {
+//         submissions.push(username);
+//         res.send(`<h3>Thanks, ${username}! </h3><a href="/">Go Back</a>`)
+//     } else {
+//         res.status(400).send('Name is required. ');
+//     }
+
+// });
+//Handle form POST and stored in in-memory file 
+app.post('/submit', (req,res) => {
     const username = req.body.username;
-    if (username) {
-        submissions.push(username);
-        res.send(`<h3>Thanks, ${username}! </h3><a href="/"></a>`)
+    if (username){
+        const entry = `${username}\n`;
+        fs.appendFile('data.txt', entry, (err) => {
+            if(err) {
+                console.error('Failed to write to file;', err);
+                return res.status(500).send('Something went wrong');
+            }
+            res.send(`<h3>Thanks, ${username}! </h3><a href="/">Go Back</a>`)
+        });
     } else {
-        res.status(400).send('Name is required. ');
+        res.status(400).send('Name is required');
     }
-
+    
 });
 
 app.listen(PORT, () => {
